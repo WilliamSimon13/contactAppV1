5e53616014a4e70853f7da7504f47181b39b9e34
WilliamSimon13 <maithieutin@gmail.com>
2024-03-15 08:06:24 +0700

    First Test Git

---
+++ Contact App/app/src/main/java/com/example/contactapp/ContactsAdapter.java
@@ -0,0 +1,9 @@
+package com.example.contactapp;
+
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class contactsAdepter {
+    private ArrayList<String> contactList;
+}
---
+++ playing aroung/app/src/main/java/com/example/playaround/MyViewModel.java
@@ -0,0 +1,2 @@
+package com.example.playaround;public class MyViewModel {
+}
